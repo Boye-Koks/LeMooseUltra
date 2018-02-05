ab3b66553b33703fb29a72a19fdd5f2a03801842
Boye-Koks <btkoks@gmail.com>
2018-02-05 17:05:55 +0100

    Eigen model traint nu ook

--- Model_Boye/firstmodel.py
+++ Model_Boye/firstmodel.py
@@ -2,6 +2,8 @@
 
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 
 from keras.models import Sequential
 from keras.layers import Dense
@@ -11,14 +13,27 @@ def create_model(nodelist):
     model.add(Dense(784, input_shape=(784,)))
     for nb_nodes in nodelist:
         model.add(Dense(nb_nodes))
-    model.add(Dense(10))
-    model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+    model.add(Dense(10, activation='softmax'))
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
     print(model.summary())
     return model
 
 trn_data = pd.read_csv("Model_Boye/train.csv")
 tst_data = pd.read_csv("Model_Boye/test.csv")
 trn_labels = trn_data['label'].values
-trn_img = trn_data.drop(['label'], axis=1).values
+trn_img = 1/255 * trn_data.drop(['label'], axis=1).values
 
-create_model([128,128])
+# TODO convert trn_labels to one hot encoding
+
+label_encoder = LabelEncoder()
+integer_encoded = label_encoder.fit_transform(trn_labels)
+# binary encode
+onehot_encoder = OneHotEncoder(sparse=False)
+integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+
+trn_enc_labels = onehot_encoded
+
+new_model = create_model([10, 10])
+
+new_model.fit(x=trn_img, y=trn_enc_labels, epochs=10, batch_size=128, verbose=2)
